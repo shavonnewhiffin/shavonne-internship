0c5a98370e2693e620ead13af31e5262f0afecf0
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-07-14 21:40:17 -0700

    feat: implemented skeleton loading states

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -37,28 +37,27 @@ const ExploreItems = () => {
       </div>
 
       {/* 1. Wrap the conditional rendering in a "row" div */}
-      <div className="row">
-        {loading
-          ? new Array(16).fill(0).map((_, index) => (
-              <div
-                key={index}
-                className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-                style={{ display: "block", backgroundSize: "cover" }}
-              >
-                {/* 2. Pro Tip: Ensure your CSS skeleton class has a defined height/width */}
-                <div className="exploreItems__img--skeleton skeleton" style={{ height: "350px", width: "100%", borderRadius: "8px" }} />
-              </div>
-            ))
-          : exploreItems.map((item, index) => (
-              <div
-                key={item.nftId || index}
-                className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-                style={{ display: "block", backgroundSize: "cover" }}
-              >
-                <Card {...item} />
-              </div>
-            ))}
+      {loading
+  ? new Array(16).fill(0).map((_, index) => (
+      <div
+        key={index}
+        className="d-item col-lg-3 col-md-6 col-sm-6 col-12"
+        style={{ display: "block" }}
+      >
+        <div className="nft__item">
+          <div className="exploreItems__img--skeleton skeleton" />
+        </div>
       </div>
+    ))
+  : exploreItems.map((item, index) => (
+      <div
+        key={item.nftId || index}
+        className="d-item col-lg-3 col-md-6 col-sm-6 col-12"
+        style={{ display: "block" }}
+      >
+        <Card {...item} />
+      </div>
+    ))}
 
       <div className="col-md-12 text-center">
         <Link to="" id="loadmore" className="btn-main lead">
