f59dc13913a03c74465f50908a536329072cc5b1
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-07-14 21:24:33 -0700

    working through skeleton

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -36,25 +36,29 @@ const ExploreItems = () => {
         </select>
       </div>
 
-      {loading
-        ? new Array(16).fill(0).map((_, index) => (
-            <div
-              key={index}
-              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              style={{ display: "block", backgroundSize: "cover" }}
-            >
-              <div className="exploreItems__img--skeleton skeleton" />
-            </div>
-          ))
-        : exploreItems.map((item, index) => (
-            <div
-              key={item.nftId || index}
-              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-              style={{ display: "block", backgroundSize: "cover" }}
-            >
-              <Card {...item} />
-            </div>
-          ))}
+      {/* 1. Wrap the conditional rendering in a "row" div */}
+      <div className="row">
+        {loading
+          ? new Array(16).fill(0).map((_, index) => (
+              <div
+                key={index}
+                className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                style={{ display: "block", backgroundSize: "cover" }}
+              >
+                {/* 2. Pro Tip: Ensure your CSS skeleton class has a defined height/width */}
+                <div className="exploreItems__img--skeleton skeleton" style={{ height: "350px", width: "100%", borderRadius: "8px" }} />
+              </div>
+            ))
+          : exploreItems.map((item, index) => (
+              <div
+                key={item.nftId || index}
+                className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                style={{ display: "block", backgroundSize: "cover" }}
+              >
+                <Card {...item} />
+              </div>
+            ))}
+      </div>
 
       <div className="col-md-12 text-center">
         <Link to="" id="loadmore" className="btn-main lead">
