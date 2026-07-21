0c144f19f7e74ab9abe8555d771839a0e0bea951
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-07-20 20:56:34 -0700

    fix: cleaned code - removed stray console.log

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -11,12 +11,12 @@ const TopSellers = () => {
       const { data } = await axios.get(
         "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
       );
-      console.log(data);
       setTopSellers(data);
       setLoading(false);
     } catch {
-      setLoading(false);
       console.error("We had trouble pulling the Top Sellers");
+    } finally {
+      setLoading(false);
     }
   }
 
