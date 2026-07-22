c91d9b7279b1af482896027a9188eb13b8264f83
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-07-21 19:14:38 -0700

    feat: implemented data- aos animations

--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -7,10 +7,10 @@ const LandingIntro = () => {
         <div className="row">
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_wallet"></i>
+              <i data-aos="fade-in" data-aos-delay="100" data-aos-duration="1000" className="bg-color-2 i-boxed icon_wallet"></i>
               <div className="text">
-                <h4 className="">Set up your wallet</h4>
-                <p>
+                <h4 data-aos="fade-in" data-aos-delay="200" data-aos-duration="1000" className="">Set up your wallet</h4>
+                <p data-aos="fade-up" data-aos-delay="300" data-aos-duration="1000">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
@@ -20,10 +20,10 @@ const LandingIntro = () => {
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
+              <i data-aos="fade-in" data-aos-delay="100" data-aos-duration="1000" className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
               <div className="text">
-                <h4 className="">Add your NFT's</h4>
-                <p>
+                <h4 data-aos="fade-in" data-aos-delay="200" data-aos-duration="1000" className="">Add your NFT's</h4>
+                <p data-aos="fade-up" data-aos-delay="300" data-aos-duration="1000">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
@@ -33,10 +33,10 @@ const LandingIntro = () => {
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_tags_alt"></i>
+              <i data-aos="fade-in" data-aos-delay="100" data-aos-duration="1000" className="bg-color-2 i-boxed icon_tags_alt"></i>
               <div className="text">
-                <h4 className="">Sell your NFT's</h4>
-                <p>
+                <h4 data-aos="fade-in" data-aos-delay="200" data-aos-duration="1000" className="">Sell your NFT's</h4>
+                <p data-aos="fade-up" data-aos-delay="300" data-aos-duration="1000">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
